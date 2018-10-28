c0358cb4f9c0b1466f06c8c4273e9d070dc68393
Wout <git@wout.maaskant.info>
2018-10-28 15:46:30 +0100

    fix: Small Duplicacy fixes.

--- desktop/backup.cmd
+++ desktop/backup.cmd
@@ -3,5 +3,5 @@ cls
 echo Backing up
 setlocal
 call config.cmd
-pushd c: & %DUPLICACY% -log backup -dry-run -stats & popd
-pushd d: & %DUPLICACY% -log backup -dry-run -stats & popd
+pushd d:\ & %DUPLICACY% -log backup -threads 8 -stats & popd
+pushd c:\ & %DUPLICACY% -log backup -threads 4 -stats & popd
