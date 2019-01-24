97807f266e3cbb823ee86e5a289de1545ca16345
Wout <git@wout.maaskant.info>
2019-01-24 13:28:55 +0100

    feat: Script to restore backups

--- desktop/backup.cmd
+++ desktop/backup.cmd
@@ -4,4 +4,4 @@ echo Backing up
 setlocal
 call config.cmd
 pushd d:\ & %DUPLICACY% -log backup -threads 8 -stats & popd
-pushd c:\ & %DUPLICACY% -log backup -threads 4 -stats & popd
+pushd c:\ & %DUPLICACY% -log backup -threads 8 -stats & popd
