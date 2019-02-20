5b1752b3acf7a461ba594b2216e3d721a25801ea
scheleaap <git@wout.maaskant.info>
2019-02-20 14:50:55 +0100

    feat: Duplicacy on Linux

--- server/linux-duplicacy/backup.sh
+++ server/linux-duplicacy/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Backing up"
 source /opt/backups/server/linux-duplicacy/config
-(cd / && duplicacy -log -d backup -threads 8 -stats --dry-run)
+(cd / && duplicacy -log -d backup -threads 8 -stats)
