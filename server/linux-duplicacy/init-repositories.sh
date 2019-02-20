6b331dbab9dc94c561dd3907dc3448fff624895a
scheleaap <git@wout.maaskant.info>
2019-02-20 14:50:13 +0100

    feat: Duplicacy on Linux

--- server/linux-duplicacy/init-repositories.sh
+++ server/linux-duplicacy/init-repositories.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Initializing repositories
 source /opt/backups/server/linux-duplicacy/config
-duplicacy init -encrypt -pref-dir /opt/backups/server/linux-duplicacy/.duplicacy-$DUPLICACY_REPOSITORY_ID -repository / $DUPLICACY_REPOSITORY_ID b2://$DUPLICACY_B2_BUCKET
+(cd / && duplicacy init -encrypt -pref-dir /opt/backups/server/linux-duplicacy/.duplicacy-$DUPLICACY_REPOSITORY_ID -repository / $DUPLICACY_REPOSITORY_ID b2://$DUPLICACY_B2_BUCKET)
