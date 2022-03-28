9ce811934bc200f9155f65ed35588ae9428d0e41
scheleaap <git@wout.maaskant.info>
2022-03-28 19:03:34 +0200

    feat(desktop-restic): Cleanup

--- desktop/restic/cleanup.cmd
+++ desktop/restic/cleanup.cmd
@@ -4,6 +4,5 @@ echo Cleaning up
 setlocal
 call config.cmd
 
-restic -r b2:%B2_BUCKET%:scheleaap/d --verbose forget --keep-last 1 --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --dry-run
-:: restic -r b2:%B2_BUCKET%:scheleaap/d --verbose prune
-:: restic -r b2:%B2_BUCKET%:scheleaap/d --verbose check
+restic -r b2:%B2_BUCKET%:scheleaap/d --verbose forget --prune --keep-last 1 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
+restic -r b2:%B2_BUCKET%:scheleaap/d --verbose check
