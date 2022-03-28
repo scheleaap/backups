0d3b9e821655e8b8ed63cc58bf5005e6e190d66b
scheleaap <git@wout.maaskant.info>
2022-03-28 18:56:14 +0200

    feat(linux-restic): Cleanup

--- server/linux-restic/cleanup.sh
+++ server/linux-restic/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Cleaning up"
-source /opt/backups/server/linux-restic/config
-
-TODO
+basedir=/opt/backups/server/linux-restic
+source $basedir/config
+/opt/restic/restic -r b2:${B2_BUCKET}:aapserver --verbose forget --keep-last 1 --keep-daily 7 --keep-weekly 4 --keep-monthly 12
