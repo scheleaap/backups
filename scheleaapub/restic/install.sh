ea2b1bd44fa3c43fb0171d4bdc10445ab8a36a68
scheleaap <git@wout.maaskant.info>
2024-10-26 15:43:50 +0200

    feat(scheleaapub): Add systemctl service and timer

--- scheleaapub/restic/install.sh
+++ scheleaapub/restic/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 sudo apt-get install restic
+
+sudo mkdir -p /usr/local/lib/systemd/system
+sudo ln -s /home/wout/dev/backups/scheleaapub/restic/backup.service /usr/local/lib/systemd/system/backup.service
+sudo ln -s /home/wout/dev/backups/scheleaapub/restic/backup.timer /usr/local/lib/systemd/system/backup.timer
+sudo systemctl enable backup.timer
