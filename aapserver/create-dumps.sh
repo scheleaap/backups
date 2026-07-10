a10d0bd8f87b55cb479c163e61848a8c9bd92f8a
scheleaap <git@wout.maaskant.info>
2026-07-10 19:29:46 +0200

    fix(aapserver): Replace usage of 'apt' in script to create dumps

--- aapserver/create-dumps.sh
+++ aapserver/create-dumps.sh
@@ -4,5 +4,6 @@ dump_directory="${1:-/var/backups/custom-dumps}"
 
 mkdir -p "${dump_directory}"
 
-apt list --installed > "${dump_directory}/apt-list-installed.txt"
+#apt list --installed > "${dump_directory}/apt-list-installed.txt"
+dpkg-query -W -f '${binary:Package}\t${Version}\t${Status}\n' | awk -F'\t' '$3 == "install ok installed"' > "${dump_directory}/dpkg-installed.txt"
 apt-mark showmanual > "${dump_directory}/apt-mark-showmanual.txt"
